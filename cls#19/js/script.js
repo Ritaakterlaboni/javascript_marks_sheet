d147ce56861d09fcb3d90d66bc6a45bc155edf24
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2024-09-03 22:47:37 +0600

    javascript_marks

---
+++ cls#19/js/script.js
@@ -0,0 +1,25 @@
+//console.log("ami vlo na")               //inspect a giya then console a dekte parbo
+
+//document.write("ami vlo na")           // index a dekhate chaile ai formula use korte hobe
+
+let marks=96
+
+if(marks >=33 && marks <49){
+    console.log("u got C")
+}
+else if(marks >=50 && marks <59){
+    console.log("u got B")
+}
+else if(marks >=60 && marks <69){
+    console.log("u got A-")
+}
+else if(marks >=70 && marks <79){
+    console.log("u got A")
+}
+else if(marks >=80 && marks <100){
+    console.log("u got A+")
+}
+
+else{
+    console.log("failed")
+}
